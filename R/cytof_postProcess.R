955add978ffa7c5e2e388012e4edc5ca99a8b113
SamGG <samuel.granjeaud@inserm.fr>
2024-09-29 01:03:35 +0200

    Simplify cytof_addToFCS(): store cluster ID (with a little of randomness) and reduced dimensions in original (linear) scales.

--- R/cytof_postProcess.R
+++ R/cytof_postProcess.R
@@ -204,9 +204,7 @@ cytof_writeResults <- function(analysis_results,
                        origSampNames = samples,
                        analyzedFCSdir = paste(projectName, "analyzedFCS", sep = "_"), 
                        transformed_cols = trans_col_names, 
-                       cluster_cols = cluster_col_names,
-                       clusterIDs = ctols,
-                       inLgclTrans = inverseLgclTrans)
+                       cluster_cols = cluster_col_names)
     }
     setwd(curwd)
     
@@ -818,190 +816,72 @@ cytof_progressionPlot <- function(data, markers, clusters,
 #' Store the new dimension transformed data and cluster data into the exprs 
 #' matrix in new fcs files under \code{analyzedFCSdir}
 #' 
-#' @param data The new data matrix to be added in.
+#' @param to_add The data matrix to add/append as columns/channels.
 #' @param rawFCSdir The directory containing the original fcs files.
 #' @param origSampNames Vector of original names of samples, if samples were renamed.
 #' @param analyzedFCSdir The directory to store the new fcs files.
 #' @param transformed_cols The column name of the dimension transformed data in \code{data}.
 #' @param cluster_cols The column name of the cluster data in \code{data}.
-#' @param clusterIDs Table of cell cluster IDs for each clustering method
-#' @param specifySampleNames Used only if sample names differ from those in raw fcs.
-#' @param inLgclTrans If \verb{TRUE}, apply the inverse lgcl transformation to the cluster data before saving
 #' 
 #' @export
 #' 
 #' @return New fcs files stored under \code{analyzedFCSdir}
 #' @importFrom flowCore keyword keyword<-
 #' @importFrom Biobase  exprs exprs<- description description<- pData pData<- 
-cytof_addToFCS <- function(data, 
-                           rawFCSdir,
-                           origSampNames = NULL,
-                           analyzedFCSdir, 
-                           transformed_cols = c("tsne_1", "tsne_2"), 
-                           cluster_cols = c("cluster"),
-                           clusterIDs = NULL,
-                           specifySampleNames = NULL,
-                           inLgclTrans = TRUE) {
-    
-    lgcl <- logicleTransform(w = 0.1, t = 4000, m = 4.5, a = 0)
-    ilgcl <- inverseLogicleTransform(trans = lgcl)
+cytof_addToFCS <- function(
+        to_add, 
+        rawFCSdir,
+        origSampNames = NULL,
+        analyzedFCSdir, 
+        transformed_cols = c("tsne_1", "tsne_2"), 
+        cluster_cols = c("cluster")
+) {
     
     if (!dir.exists(analyzedFCSdir)) {
-        dir.create(analyzedFCSdir)
+        dir.create(analyzedFCSdir, recursive = TRUE)
     }
     
-    ## transform transformed_cols
-    if (!is.null(transformed_cols)) {
-        transformed <- data[, transformed_cols, drop=FALSE]
-        
-        ## ilgcl transformation of t-SNE
-        N_transformed <- apply(transformed, 2, function(x) ((x-min(x))/(max(x)-min(x)))*4.4)
-        R_N_transformed <- apply(N_transformed,2,ilgcl)
-        ## linear transformation of tSNE
-        R_N_transformed_l <- apply(transformed, 2, function(x) (x - min(x)) + 0.1)  
-        colnames(R_N_transformed_l) <- paste0(colnames(R_N_transformed_l), "_linear")
-        
-        ## output both ilgcl and linear transformaton of tSNE for visualization on flowJo
-        R_N_transformed <- cbind(R_N_transformed, R_N_transformed_l)
-        row.names(R_N_transformed) <- row.names(data)
+    # add some randomness cluster_cols
+    for (i in cluster_cols){
+        to_add[, i] <- to_add[, i] +
+            asin(runif(nrow(to_add), -1, 1))/pi/2
     }
-    
-    ## use original sample names
-    if (!is.null(specifySampleNames)) {
-      originalSample <- specifySampleNames
-    }
-    
-    ## transform cluster_cols
-    if (!is.null(cluster_cols)) {
-        if(inLgclTrans){
-            for(i in 1:length(cluster_cols)){
-                cCol <- data[, cluster_cols[i]]
-                clust_cor_1 <- as.numeric(cCol)%%10
-                clust_cor_2 <- floor(as.numeric(cCol)/10)
-                clust_cor_1 <- clust_cor_1 + runif(length(clust_cor_1), 0, 0.2)
-                clust_cor_2 <- clust_cor_2 + runif(length(clust_cor_2), 0, 0.2)
-                cluster_cor12 <- cbind(clust_cor_1, clust_cor_2)
-                N_clust_cor <- apply(cluster_cor12, 2, function(x) ((x - min(x))/(max(x) - min(x))) * 4.4)
-                clust_cor <- apply(N_clust_cor, 2, ilgcl)
-                colnames(clust_cor) <- paste(cluster_cols[i], c("cor_1", "cor_2"), sep="_") 
-                if(i == 1){
-                    R_N_clust_cor <- clust_cor
-                }else{
-                    R_N_clust_cor <- cbind(R_N_clust_cor, clust_cor)
-                }
-            }
-            
-        }else{
-            R_N_clust_cor <- data[, cluster_cols, drop = FALSE]
-        }
-        
-        row.names(R_N_clust_cor) <- row.names(data)
-    }
-    
-    ## write data to FCS
-    if ((!is.null(transformed_cols)) && (!is.null(cluster_cols))) {
-        to_add <- cbind(R_N_transformed, R_N_clust_cor)
-    } else if (!is.null(transformed_cols)) {
-        to_add <- R_N_transformed
-    } else if (!is.null(cluster_cols)) {
-        to_add <- R_N_clust_cor
-    }
-    if(!is.null(clusterIDs)){
-      colnames(clusterIDs) <- paste0(colnames(clusterIDs), "_clusterIDs")
-      to_add <- cbind(to_add, clusterIDs)
-    }
-    addColNames <- colnames(to_add)
-    sample <- unique(sub("_[0-9.]+$", "", row.names(to_add)))
-    # add argument for old sample names use match()
-    
-    sample_key <- factor(gsub("_[0-9]+$", "", row.names(to_add)))
-    cellno_key <- as.integer(gsub(".+_([0-9]+$)", "\\1", row.names(to_add)))
+
+    # split sample_cellno key
+    sample <- sub("_[0-9.]+$", "", rownames(to_add))
+    sample_key <- factor(sample, levels = unique(sample))
+    sample <- levels(sample_key)
+    cellno_key <- as.integer(gsub(".+_([0-9]+$)", "\\1", rownames(to_add)))
+    # 
     for (i in 1:length(sample)) {
-      # refer to old sample name
-      if(!is.null(origSampNames)){
-        fn <- paste0(rawFCSdir, .Platform$file.sep, origSampNames[i], ".fcs")
-      }else{
-        fn <- paste0(rawFCSdir, .Platform$file.sep, sample[i], ".fcs")
-      }
-    	if(!file.exists(fn)){
-    	    ## stop the writing if cannot find the file
-    	    message(paste("Cannot find raw FCS file:", fn))
-    	    next()
-    	}
-        cat("Processing file:", basename(fn), "\n")
-        fcs <- read.FCS(fn, transformation = FALSE)
-        to_add_i <- to_add[sample[i] == sample_key, ]
-        cellNo_i <- cellno_key[sample[i] == sample_key]
-        sub_exprs <- fcs@exprs[cellNo_i, ]
-        params <- parameters(fcs)
-        pd <- pData(params)
-        keyval <- keyword(fcs)
-        
-        for (j in 1:length(addColNames)) {
-            ## update the parameters
-            if (addColNames[j] %in% colnames(fcs@exprs)) {
-                addColNames[j] <- paste(addColNames[j], "_new", sep = "")
-            }
-            
-            addColName <- addColNames[j]
-            channel_number <- nrow(pd) + 1
-            channel_id <- paste("$P", channel_number, sep = "")
-            channel_name <- addColName
-            minRange <- floor(min(to_add_i[,j]))
-            maxRange <- ceiling(max(to_add_i[,j]))
-            # channel_range <- maxRange - minRange
-            channel_range <- maxRange + 1
-            
-            plist <- matrix(c(channel_name, "<NA>", channel_range, 
-                minRange, maxRange))
-            rownames(plist) <- c("name", "desc", "range", "minRange", 
-                "maxRange")
-            colnames(plist) <- c(channel_id)
-            pd <- rbind(pd, t(plist))
-            
-            ## update the expression value
-            out_col_names <- colnames(sub_exprs)
-            sub_exprs <- cbind(sub_exprs, to_add_i[,j])
-            colnames(sub_exprs) <- c(out_col_names, addColName)
-            
-            ## update the description remove '\' in the keywords
-            keyval <- lapply(keyval, function(x) {
-                if (class(x) == "character") {
-                  gsub("\\", "", x, fixed = TRUE)
-                } else {
-                  x
-                }
-            })
-            keyval[[paste0("$P", channel_number, "B")]] <- "32"  # Number of bits
-            keyval[[paste0("$P", channel_number, "R")]] <- toString(channel_range)  # Range
-            keyval[[paste0("$P", channel_number, "E")]] <- "0,0"  # Exponent
-            keyval[[paste0("$P", channel_number, "N")]] <- channel_name  # Name
-            # The following keywords are not essential
-            # keyval[[paste0("P", channel_number, "BS")]] <- 0
-            # keyval[[paste0("P", channel_number, "MS")]] <- 0
-            # keyval[[paste0("P", channel_number, "DISPLAY")]] <- "LIN"  # data display
-            #add keyval for clusterID and annotation
+        # refer to old sample name
+        if(!is.null(origSampNames)){
+            fn <- paste0(rawFCSdir, .Platform$file.sep, origSampNames[i], ".fcs")
+        }else{
+            fn <- paste0(rawFCSdir, .Platform$file.sep, sample[i], ".fcs")
         }
-        
+        # skip if cannot find the file
+        if(!file.exists(fn)){
+            warning("Cannot find raw FCS file: ", fn)
+            next()
+        }
+        cat("Processing file:", basename(fn), "\n")
+        # identify rows to extract from the result
+        to_add_i <- to_add[sample[i] == sample_key, ]
+        cellno_i <- cellno_key[sample[i] == sample_key]
+        # read the FCS file
+        fcs <- cytof_readFCS(fn, comp = FALSE, verbose = FALSE)
+        # extract the subset of cells
+        fcs <- fcs[cellno_i, ]
+        # update the expression value
+        out_frame <- flowCore::fr_append_cols(fcs, to_add_i)
         # Remove flowCore keywords
-        kwd_flowcore <- grep("^flowCore_\\$P", names(keyval))
-        kwd_flowcore <- c(kwd_flowcore, grep("^transformation$", names(keyval)))
-        if (length(kwd_flowcore)) keyval <- keyval[-kwd_flowcore]
-
-        pData(params) <- pd
-        out_frame <- flowFrame(exprs = sub_exprs, parameters = params, description = keyval)
-
-        # ReRemove flowCore keywords
         keyval <- keyword(out_frame)
         kwd_flowcore <- grep("^flowCore_\\$P", names(keyval))
         kwd_flowcore <- c(kwd_flowcore, grep("^transformation$", names(keyval)))
         if (length(kwd_flowcore)) keyword(out_frame) <- keyval[-kwd_flowcore]
-        
-        #### similar to changes made on 23 Jan 2019 by jinmiao chen
-        #### to ensure tsne and clustering show on flowjo
-        #out_frame <- out_frame[,c((ncol(fcs@exprs)+1):ncol(out_frame@exprs),1:ncol(fcs@exprs))]
-        ####
-        
-        suppressWarnings(write.FCS(out_frame, paste0(analyzedFCSdir, "/cytofkit_", sample[i], ".fcs")))
+        # write to disk
+        suppressWarnings(write.FCS(out_frame, file.path(
+            analyzedFCSdir, sprintf("ck_%s.fcs", sample[i]))))
     }
 } 
